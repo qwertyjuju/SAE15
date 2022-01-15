0828eb0c495b4ffd2fb969b9207e14ed471a03b6
qwertyjuju <70114133+qwertyjuju@users.noreply.github.com>
2022-01-15 14:59:03 +0100

    commit

--- projet/SAE15.py
+++ projet/SAE15.py
@@ -32,15 +32,14 @@ def parse_header():
     pass
 
 def is_time(part):
-    """
-    
-    """
     if part.count(":")==2 and part.count(".")==1:
-        for subpart in re.split(": |.") :
+        for subpart in re.split(r'\W+',part) :
             try:
                 int(subpart)
             except ValueError:
                 return 0
+            else:
+                pass
         return 1
     else:
         return 0
@@ -53,44 +52,61 @@ def is_comparator(part):
     if part == ">":
         return 1
     
+def is_hexa(part):
+    if part.count("x") and part[:1] == "0x":
+        return 1
+    
+def is_flag(part):
+    if part.lower()=="flags":
+        return 1
+
 def interpret_part(part):
     if is_time(part):
         return "time"
     if is_comparator(part):
         return "comparator"
-    if part.count("x") and part[:1] == "0x":
+    if is_flag(part):
+        return "flags"
+    if is_hexa(part):
         return "hexa"
     
+def create_header(line):
+    linesplit = line.split(" ")
+    attributes=[]
+    values[]
+    for partindex,part in enumerate(linesplit):
+        part_type = interpret_part(part)
+        if part_type=="time":
+            result[0]=part
+        if part_type=="comparator":
+            result[1]= linesplit[partindex-1]
+            result[2]= linesplit[partindex+1]
+        if part_type =="flags":
+            result[3]= linesplit[partindex+1]
     
-    
-def interpret_line(line):
+def interpret_line(line, nbtest=2):
     linetype = None
     linesplit = line.split(" ")
-    result = None
-    for part in linesplit:
-        if interpret_part(part) in HEADER_CONTENT:
-            linetype = "header"
-        result = [None]*8
-        for i in linesplit[1]:
-            result[0]=linesplit[0]
-            try :
-                index = linesplit.index(">")
-            except ValueError:
-                pass
-            else:
-                result[1]= linesplit[index-1]
-                result[2]= linesplit[index+1]
-            
-    if interpret_part(linesplit[0]) == "hexa":
-        linetype = "content"
-    return linetype, result
+    header_count=0
+    content_count=0
+    for testnb in range(nbtest):
+        part_type = interpret_part(linesplit[testnb])
+        if part_type in HEADER_CONTENT:
+            header_count+=1
+        if part_type in CONTENT_CONTENT:
+            content_count+=1
+    if header_count> content_count:
+        linetype = "header"
+    if content_count> header_count:
+            linetype = "content"
+    return linetype
         
 def main(filename):
     file = read_file(filename)
     for line in file:
-        line_type, result = interpret_line(line)
+        line_type = interpret_line(line)
         if line_type == "header":
-            print(result)
+            create_header(line)
             
 """
 header content:[time, IP emetor, IP receptor, TCP flag, seq, ack, window size, length]
